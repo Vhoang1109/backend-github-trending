34e4799cd845a98cd0932040ab1490d415a61f0c
vhoang1109 <vhoang1928@gmail.com>
2024-10-18 13:16:55 +0700

    update api.go

--- router/api.go
+++ router/api.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"backend-github-trending/handler"
+	middleware "backend-github-trending/middleware"
 
 	"github.com/labstack/echo"
 )
@@ -12,7 +13,9 @@ type API struct {
 }
 
 func (api *API) SetupRouter() {
-
 	api.Echo.POST("/user/sign-in", api.UserHandler.HandleSignIn)
 	api.Echo.POST("/user/sign-up", api.UserHandler.HandleSignUp)
+
+	// Using a different route for Profile and applying the JWT middleware correctly
+	api.Echo.GET("/user/profile", api.UserHandler.Profile, middleware.JWTMiddleware())
 }
